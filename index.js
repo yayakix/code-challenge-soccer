768b711c8f5a1abcd04068250e0ddca5a7bff035
Iyana Marquez <yayakix@gmail.com>
2022-08-28 21:06:09 -0400

    adding comments to formatInput function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inputStr = `San Jose Earthquakes 3, Santa Cruz Slugs 3
+const testInput = `San Jose Earthquakes 3, Santa Cruz Slugs 3
 Capitola Seahorses 1, Aptos FC 0
 Felton Lumberjacks 2, Monterey United 0
 Felton Lumberjacks 1, Aptos FC 2
@@ -11,16 +11,25 @@ Aptos FC 2, Monterey United 0
 Capitola Seahorses 5, San Jose Earthquakes 5
 Santa Cruz Slugs 1, Felton Lumberjacks 1`;
 
-console.log(
-  inputStr.split("\n").map((item) => {
+// Takes in a string of text and returns nested array of games
+function formatInput(inputStr) {
+  // split by line/game
+  return inputStr.split("\n").map((item) => {
+    // split by comma/team
     return item.split(",").map((item) => {
-      let name = "";
-      let score = 0;
-      let lastIndex = item.lastIndexOf(" ");
-      name = item.slice(0, lastIndex).trim();
-      score = item.slice(lastIndex + 1);
+        // get index of split between name and score
+      const lastIndex = item.lastIndexOf(" ");
+      const name = item.slice(0, lastIndex).trim();
+      const score = item.slice(lastIndex + 1);
 
       return [name, score];
     });
-  })
-);
+  });
+};
+
+function solutionFunc() {
+  console.log(formatInput(testInput));
+};
+
+
+solutionFunc();
