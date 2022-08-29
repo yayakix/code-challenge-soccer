6e374d7b384e0ef0a41bad16645fb241bd812b02
Iyana Marquez <yayakix@gmail.com>
2022-08-28 21:25:51 -0400

    created main loop

--- index.js
+++ index.js
@@ -9,15 +9,32 @@ function formatInput(inputStr) {
       // get index of split between name and score
       const lastIndex = item.lastIndexOf(" ");
       const name = item.slice(0, lastIndex).trim();
-      const score = item.slice(lastIndex + 1);
+      const score = parseInt(item.slice(lastIndex + 1));
 
       return [name, score];
     });
   });
 }
 
-function solutionFunc() {
-  console.log(formatInput(testInput));
+function solutionFunc(input) {
+  const formattedInput = formatInput(input);
+  let seenTeams = [];
+  let currentDay = 1;
+  formattedInput.forEach((game) => {
+    const teamOne = game[0];
+    const teamTwo = game[1];
+    console.log("TEAM ONE IS", teamOne);
+    console.log("TEAM TWO IS", teamTwo);
+  });
+  //   console.log(formattedInput);
+  //   for loop,
+  // make sure each name is unique- if not, then end of matchday
+  //
+  //   separate top 3 scores in each matchday
+  //
+  //   map of teams with scores
+
+  //
 }
 
-solutionFunc();
+solutionFunc(testInput);
