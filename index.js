e2d18c67025449d1ed84067eb9e9011d6ae2557e
Iyana Marquez <yayakix@gmail.com>
2022-08-28 21:11:22 -0400

    added sample input file

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const testInput = `San Jose Earthquakes 3, Santa Cruz Slugs 3
-Capitola Seahorses 1, Aptos FC 0
-Felton Lumberjacks 2, Monterey United 0
-Felton Lumberjacks 1, Aptos FC 2
-Santa Cruz Slugs 0, Capitola Seahorses 0
-Monterey United 4, San Jose Earthquakes 2
-Santa Cruz Slugs 2, Aptos FC 3
-San Jose Earthquakes 1, Felton Lumberjacks 4
-Monterey United 1, Capitola Seahorses 0
-Aptos FC 2, Monterey United 0
-Capitola Seahorses 5, San Jose Earthquakes 5
-Santa Cruz Slugs 1, Felton Lumberjacks 1`;
+const testInput = require("fs").readFileSync("sample-input.txt", "utf8");
 
 // Takes in a string of text and returns nested array of games
 function formatInput(inputStr) {
@@ -17,7 +6,7 @@ function formatInput(inputStr) {
   return inputStr.split("\n").map((item) => {
     // split by comma/team
     return item.split(",").map((item) => {
-        // get index of split between name and score
+      // get index of split between name and score
       const lastIndex = item.lastIndexOf(" ");
       const name = item.slice(0, lastIndex).trim();
       const score = item.slice(lastIndex + 1);
@@ -25,11 +14,10 @@ function formatInput(inputStr) {
       return [name, score];
     });
   });
-};
+}
 
 function solutionFunc() {
   console.log(formatInput(testInput));
-};
-
+}
 
 solutionFunc();
